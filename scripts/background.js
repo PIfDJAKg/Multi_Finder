02da7c102a3c62679871f0465f16c970e6dace29
Pidjak <pifdjak@gmail.com>
2025-11-06 01:35:09 +0300

    1.1.0 Multi Finder
    Chenged:
    + New Popup Design
    
    Added
    + Wikipedia
    + StackOverflow
    + PyPI
    + Reddit
    + Pinterest

--- scripts/background.js
+++ scripts/background.js
@@ -1,16 +1,75 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
-        id: "searchGithub",
+        id: "github",
         title: "Find on Github",
         type: "normal",
         contexts: ["selection"]
     });
+    
+    chrome.contextMenus.create({
+        id: "wikipedia",
+        title: "Find on Wiki",
+        type: "normal",
+        contexts: ["selection"]
+    });
+    
+    chrome.contextMenus.create({
+        id: "stackoverflow",
+        title: "Find on StackOverflow",
+        type: "normal",
+        contexts: ["selection"]
+    })
+
+    chrome.contextMenus.create({
+        id: "pypi",
+        title: "Find on PyPI",
+        type: "normal",
+        contexts: ["selection"]
+    })
+
+    chrome.contextMenus.create({
+        id: "reddit",
+        title: "Find on Reddit",
+        type: "normal",
+        contexts: ["selection"]
+    })
+    
+    chrome.contextMenus.create({
+        id: "pinterest",
+        title: "Find on Pinterest",
+        type: "normal",
+        contexts: ["selection"]
+    })
+
+    
 });
 
 chrome.contextMenus.onClicked.addListener((info) => {
-    if (info.menuItemId === "searchGithub"){
-        const query = info.selectionText;
-        const url = `https://github.com/search?q=${encodeURIComponent(query)}&type=repositories`;
+    const query = info.selectionText
+    var url = ``;
+    
+    switch (info.menuItemId) {
+        case "github":
+            url = `https://github.com/search?q=${encodeURIComponent(query)}&type=repositories`;
+            break;
+        case "wikipedia":
+            url = `https://wikipedia.org/w/index.php?search=${encodeURIComponent(query)}`;
+            break;
+        case "pinterest":
+            url = `https://www.pinterest.com/search/pins/?q=${encodeURIComponent(query)}&rs=typed`;
+            break;
+        case "pypi":
+            url = `https://pypi.org/search/?q=${encodeURIComponent(query)}`;
+            break;
+        case "stackoverflow":
+            url = `https://stackoverflow.com/search?q=${encodeURIComponent(query)}`;
+            break;
+        case "reddit":
+            url = `https://www.reddit.com/search/?q=${encodeURIComponent(query)}`;
+            break;
+    }
+
+    if (url != ``){
         chrome.tabs.create({ url })
     }
 });
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,40 @@
 function searchingOnGithub() {
     const editTextLable = document.getElementById("searchTextEdit");
     if (editTextLable) {
-        const text_value = editTextLable.value
-        if (text_value != "") {
-            const url = `https://github.com/search?q=${encodeURIComponent(text_value)}&type=repositories`;
-            chrome.tabs.create({ url });
+        const query = editTextLable.value
+        if (query != "") {
+            const selectedOption = selectOptions_select.value
+            var url = ``;
+            switch (selectedOption) {
+                case "github":
+                    url = `https://github.com/search?q=${encodeURIComponent(query)}&type=repositories`;
+                    break;
+                case "wikipedia":
+                    url = `https://wikipedia.org/w/index.php?search=${encodeURIComponent(query)}`;
+                    break;
+                case "pinterest":
+                    url = `https://www.pinterest.com/search/pins/?q=${encodeURIComponent(query)}&rs=typed`;
+                    break;
+                case "pypi":
+                    url = `https://pypi.org/search/?q=${encodeURIComponent(query)}`;
+                    break;
+                case "stackoverflow":
+                    url = `https://stackoverflow.com/search?q=${encodeURIComponent(query)}`;
+                    break;
+                case "reddit":
+                    url = `https://www.reddit.com/search/?q=${encodeURIComponent(query)}`;
+                    break;
+            }
+            
+            if (url != ``){
+                chrome.tabs.create({ url });
+            }
         }
     }
 }
 
 const searchButton_btn = document.getElementById("searchButton");
+const selectOptions_select = document.getElementById("selectValue");
 
 if (searchButton_btn) {
     searchButton_btn.addEventListener("click", searchingOnGithub)
